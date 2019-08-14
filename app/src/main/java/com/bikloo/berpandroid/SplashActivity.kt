3cdbe1f253f52ed63f63572c6271999b132087b5
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-08-13 20:22:12 -0400

    [Tarlochan] added Splash Screen

---
+++ app/src/main/java/com/bikloo/berpandroid/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.bikloo.berpandroid
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
+    }
+}
