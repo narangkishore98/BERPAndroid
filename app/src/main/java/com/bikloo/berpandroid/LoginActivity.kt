cf3186485e78f1def6cf7b3e60670899345f5ed7
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-08-15 18:39:11 -0400

    [Tarlochan] Added Save Me Function By switch remember Me code using Shared Preferences

--- app/src/main/java/com/bikloo/berpandroid/LoginActivity.kt
+++ app/src/main/java/com/bikloo/berpandroid/LoginActivity.kt
@@ -1,21 +1,54 @@
 package com.bikloo.berpandroid
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var mSharedpreferences: SharedPreferences
+    lateinit var mEditor: SharedPreferences.Editor
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        if (switchRememberMe.isChecked() == true) {
+            getRememberMe()
+        }
     }
 
-    fun goToSignUp(view : View) {
+    fun goToSignUp(view : View)
+    {
 
         val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
         startActivity(intent)
         finish()
     }
+
+
+    //Remember Me to save
+    private fun saveRememeberMe() {
+        mSharedpreferences = getSharedPreferences("MyPREFERENCES", Context.MODE_PRIVATE)
+        mEditor = mSharedpreferences.edit()
+        mEditor.putString("email", edtEmail.text!!.toString())
+        mEditor.putString("password", edtPassword.text!!.toString())
+        mEditor.commit()
+    }
+
+    //Remember Me Empty code
+    private fun saveRememeberMeEmpty() {
+        mSharedpreferences = getSharedPreferences("MyPREFERENCES", Context.MODE_PRIVATE)
+        mEditor = mSharedpreferences.edit()
+        mEditor.putString("email", "")
+        mEditor.putString("password", "")
+        mEditor.commit()
+    }
+
+
 }
