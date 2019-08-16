82c2671268ead6a102ef91daf157da0649b530c0
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-08-16 14:20:07 -0400

    [Tarlochan] Modified Owner Class

--- app/src/main/java/com/bikloo/berpandroid/Classes/Owner.kt
+++ app/src/main/java/com/bikloo/berpandroid/Classes/Owner.kt
@@ -2,16 +2,21 @@ package com.bikloo.berpandroid.Classes
 
 class Owner :User
 {
+    var myEnterprise : MutableList<Enterprise> = ArrayList()
 
-    constructor(email: String?, fullName: String?, password: String?, userType: UserType?, address: String?) : super(
+    constructor(email: String?, fullName: String?, password: String?, address: String?) : super(
         email,
         fullName,
         password,
-        userType,
+        UserType.Owner,
         address
     )
 
     override fun toString(): String {
         return super.toString()
+    }
+    fun addEnterprise(enterprise:Enterprise)
+    {
+        myEnterprise.add(enterprise)
     }
 }
