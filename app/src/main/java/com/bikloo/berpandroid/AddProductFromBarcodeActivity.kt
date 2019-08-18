0f84478810736672d90e463a57f8020d75bfcd1d
Kishore Narang <narangkishore98@gmail.com>
2019-08-18 18:28:34 -0400

    [Kishore] Added Functionality for add product

--- app/src/main/java/com/bikloo/berpandroid/AddProductFromBarcodeActivity.kt
+++ app/src/main/java/com/bikloo/berpandroid/AddProductFromBarcodeActivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.bikloo.berpandroid.Classes.Product
+import com.bikloo.berpandroid.DataBase.DataStore
 import kotlinx.android.synthetic.main.activity_add_product_from_barcode.*
 
 class AddProductFromBarcodeActivity : AppCompatActivity() {
@@ -22,10 +24,38 @@ class AddProductFromBarcodeActivity : AppCompatActivity() {
         var adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, x)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spnrProductType.adapter = adapter
+        //adding functionality for btn add product
 
         btnAddProduct.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this, "Button Is Working",Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, "Button Is Working",Toast.LENGTH_SHORT).show()
+            val productName = edtProductName.text.toString()
+            val productDescripton = edtDescription.text.toString()
+            val price = edtPrice.text.toString().toFloat()
+            val productType = getType(x[spnrProductType.selectedItemPosition])
+
+            //var p = Product(productName, txtBarcode.text.toString(), )
+            var product = Product(productName, txtBarcode.text.toString(), productDescripton, price, productType)
+            DataStore.selectedEnterprise!!.products.add(product)
+            Toast.makeText(this,"Product Saved",Toast.LENGTH_SHORT).show()
+
         })
+    }
+    fun getType( productType:String) : Product.ProductType
+    {
+        when(productType)
+        {
+            "Food" ->   Product.ProductType.Food
+            "Bakery" -> Product.ProductType.Bakery
+            "Dairy" -> Product.ProductType.Dairy
+            "Organic" -> Product.ProductType.Organic
+            "Beverages" -> Product.ProductType.Beverages
+            else ->
+            {
+                return Product.ProductType.Food
+            }
+
+        }
+        return Product.ProductType.Food
     }
     companion object
     {
