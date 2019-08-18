826de13e5dca32d61c5f9c2bfe081659882d4893
Kishore Narang <narangkishore98@gmail.com>
2019-08-18 16:00:06 -0400

    [Kishore] Added a new activity to get the data from barcode and get more details from the user.

---
+++ app/src/main/java/com/bikloo/berpandroid/AddProductFromBarcodeActivity.kt
@@ -0,0 +1,12 @@
+package com.bikloo.berpandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddProductFromBarcodeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_product_from_barcode)
+    }
+}
