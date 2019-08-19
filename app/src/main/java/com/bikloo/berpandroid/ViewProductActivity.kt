ad9081610eb6f77e9814d947604c124a6e7c15e6
Kishore Narang <narangkishore98@gmail.com>
2019-08-18 20:35:54 -0400

    [Kishore] Setup the recycler view. Ready to run

--- app/src/main/java/com/bikloo/berpandroid/ViewProductActivity.kt
+++ app/src/main/java/com/bikloo/berpandroid/ViewProductActivity.kt
@@ -1,12 +1,35 @@
 package com.bikloo.berpandroid
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import com.bikloo.berpandroid.DataBase.DataStore
+import com.bikloo.berpandroid.adapters.ProductAdapter
+import kotlinx.android.synthetic.main.activity_view_product.*
 
 class ViewProductActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_product)
+        productRecyclerView.layoutManager = LinearLayoutManager(this)
+        productRecyclerView.adapter = ProductAdapter(DataStore.selectedEnterprise!!.products)
+
+    }
+    companion object
+    {
+        fun open(withContext: Context, withData:Bundle?)
+
+        {
+            var i: Intent =  Intent(withContext, ViewProductActivity::class.java)
+            if(withData != null)
+            {
+                i.putExtras(withData)
+            }
+            withContext.startActivity(i)
+
+        }
     }
 }
